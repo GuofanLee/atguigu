2850a71d919635b107e9d588a648ac8923cc9921
GuofanLee <963225254@qq.com>
2021-04-19 23:22:49 +0800

    原生Ajax请求

--- front-end/ajax/01.express/express基本使用.js
+++ front-end/ajax/01.express/express基本使用.js
@@ -4,7 +4,7 @@ const express = require('express');
 //2、创建应用对象
 const app = express();
 
-//3、创建路由规则，request是对请求报文的封装，response是对响应报文的封装
+//3、创建GET请求路由规则，request是对请求报文的封装，response是对响应报文的封装
 app.get('/', (request, response) => {
     //设置响应
     response.send('Hello Express');
--- front-end/ajax/02.原生Ajax/server.js
+++ front-end/ajax/02.原生Ajax/server.js
@@ -4,15 +4,25 @@ const express = require('express');
 //2、创建应用对象
 const app = express();
 
-//3、创建路由规则，request是对请求报文的封装，response是对响应报文的封装
+//3、创建GET请求路由规则，request是对请求报文的封装，response是对响应报文的封装
 app.get('/server', (request, response) => {
-    //设置响应头，设置允许跨域
+    //设置响应头，允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
     //设置响应
-    response.send('Hello Ajax');
+    response.send('Hello Ajax GET');
 });
 
-//4、监听端口&启动服务
+//4、创建所有请求（POST、GET、OPTIONS等）路由规则，request是对请求报文的封装，response是对响应报文的封装
+app.all('/server', (request, response) => {
+    //设置响应头，允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    //设置响应头，允许任意请求头
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    //设置响应
+    response.send('Hello Ajax POST');
+});
+
+//5、监听端口&启动服务
 app.listen(8000, () => {
     console.log('服务已经启动，8000端口监听中......');
 });
