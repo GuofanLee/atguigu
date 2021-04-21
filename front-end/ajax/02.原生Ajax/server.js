cb6b07548c53bfc56d0f8b0395bad9fcb1692f9c
GuofanLee <963225254@qq.com>
2021-04-21 22:16:39 +0800

    原生Ajax请求

--- front-end/ajax/02.原生Ajax/server.js
+++ front-end/ajax/02.原生Ajax/server.js
@@ -42,6 +42,17 @@ app.get('/ie', (request, response) => {
     response.send('Hello IE');
 });
 
+//测试网络超时
+app.get('/timeout', (request, response) => {
+    //设置响应头，允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    //3s后响应结果
+    setTimeout(() => {
+        //设置响应
+        response.send('延时响应');
+    }, 3000);
+});
+
 //5、监听端口&启动服务
 app.listen(8000, () => {
     console.log('服务已经启动，8000端口监听中......');
