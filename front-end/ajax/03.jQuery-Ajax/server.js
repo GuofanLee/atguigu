e4e06527a5e8a038d05b11e81b40a3c6b67a34f3
GuofanLee <963225254@qq.com>
2021-05-01 14:55:26 +0800

    jQuery通用Ajax请求

--- front-end/ajax/03.jQuery-Ajax/server.js
+++ front-end/ajax/03.jQuery-Ajax/server.js
@@ -13,6 +13,20 @@ app.all('/jquery-server', (request, response) => {
     response.send(data);
 });
 
+//测试网络超时
+app.all('/jquery-timeout', (request, response) => {
+    //设置响应头，允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    //设置响应头，允许任意请求头
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    const data = {name: '尚硅谷'};
+    //3s后响应结果
+    setTimeout(() => {
+        //设置响应
+        response.send(data);
+    }, 1000);
+});
+
 //5、监听端口&启动服务
 app.listen(8000, () => {
     console.log('服务已经启动，8000端口监听中......');
