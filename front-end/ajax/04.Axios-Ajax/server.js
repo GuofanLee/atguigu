c3541db8251e57bad1fed43d5d7fa85c77f8b592
GuofanLee <963225254@qq.com>
2021-05-04 22:29:56 +0800

    Axios发送Ajax请求

--- front-end/ajax/04.Axios-Ajax/server.js
+++ front-end/ajax/04.Axios-Ajax/server.js
@@ -5,26 +5,14 @@ const express = require('express');
 const app = express();
 
 //3、创建请求路由规则，request是对请求报文的封装，response是对响应报文的封装
-app.all('/jquery-server', (request, response) => {
-    //设置响应头，允许跨域
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    const data = {name: '尚硅谷'};
-    //设置响应
-    response.send(data);
-});
-
-//测试jQuery通用Ajax请求
-app.all('/jquery-timeout', (request, response) => {
+app.all('/axios-server', (request, response) => {
     //设置响应头，允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
     //设置响应头，允许任意请求头
     response.setHeader('Access-Control-Allow-Headers', '*');
     const data = {name: '尚硅谷'};
-    //3s后响应结果
-    setTimeout(() => {
-        //设置响应
-        response.send(data);
-    }, 1000);
+    //设置响应
+    response.send(data);
 });
 
 //5、监听端口&启动服务
