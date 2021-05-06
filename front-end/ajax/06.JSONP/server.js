03b87091e8b95ea7a02e5d741e09b44e540491c7
GuofanLee <ligf@tuoxingwang.net>
2021-05-06 16:07:55 +0800

    JSONP

--- front-end/ajax/06.JSONP/server.js
+++ front-end/ajax/06.JSONP/server.js
@@ -13,9 +13,11 @@ app.all('/jsonp-server', (request, response) => {
 
 //JS原生JSONP
 app.all('/getResult', (request, response) => {
+    //获取响应回调方法名称
+    let callbackFunctionName = request.query.callback;
     const data = {name: '尚硅谷', age: 18};
-    //响应结果是对一个JS方法调用，并传入参数，这个参数就是要返回给前端的数据
-    response.send(`handle(${JSON.stringify(data)})`);
+    //响应结果是对一个JS方法调用，方法名称从url请求参数获取，方法入参就是要返回给前端的数据
+    response.send(`${callbackFunctionName}(${JSON.stringify(data)})`);
 });
 
 //5、监听端口&启动服务
